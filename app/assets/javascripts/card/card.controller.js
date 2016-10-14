b8426abcc28895046a4e9b81e083618c33bda656
aprietof <adrianprieto00@gmail.com>
2016-10-14 19:13:05 -0400

    card view and controller added

---
+++ app/assets/javascripts/card/card.controller.js
@@ -0,0 +1,15 @@
+(function() {
+
+    'use strict'
+
+    function CardController() {
+        var vm = this;
+
+
+    }
+
+    angular
+        .module('app')
+        .controller('CardController', CardController)
+
+}());
