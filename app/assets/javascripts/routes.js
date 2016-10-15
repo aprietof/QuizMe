48ee2bece1716ff83954a82626ec90db7845a803
aprietof <adrianprieto00@gmail.com>
2016-10-14 20:33:23 -0400

    extra routes added

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -25,7 +25,26 @@
                     url: 'results',
                     templateUrl: 'results/results.html',
                     controller: 'ResultsController as vm'
+                })
+                .state('card', {
+                    url: '/card/:id',
+                    templateUrl: 'card/card.html',
+                    controller: 'ListController as vm'
                 });
+            // .state('user', {
+            //     // Add Configuration Object
+            //     url: '/user/:id',
+            //     templateUrl: 'views/user.html',
+            //     controller: 'UserController as vm',
+            //     // Fetch Data
+            //     resolve: {
+            //         // Pass $http and $statPparams as arguments
+            //         user: function functionName($http, $stateParams) {
+            //             // Return response Data passing a dinamic Url
+            //             return $http.get('http://0.0.0.0:8882/rest/user/' + $stateParams.id);
+            //         }
+            //     }
+            // });
             $urlRouterProvider.otherwise('/')
         });
 }());
