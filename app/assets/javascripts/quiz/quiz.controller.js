2ec4acedb6b12c58caab4e104ac642345fa59191
aprietof <adrianprieto00@gmail.com>
2016-10-16 13:10:15 -0400

    error checking functions refactoring

--- app/assets/javascripts/quiz/quiz.controller.js
+++ app/assets/javascripts/quiz/quiz.controller.js
@@ -13,6 +13,8 @@
         vm.questions = QuizFactory.quizQuestions()
         vm.activeQuestionIndex = 0;
         vm.activeQuestion = vm.questions[0]
+        vm.error = false
+        vm.finalize = false
 
         // Callable Methods
         vm.questionAnswered = questionAnswered;
@@ -27,16 +29,19 @@
 
         function setActiveQuestion() {
             var breakOut = false;
-            var quizLengthIndexed = vm.questions.length - 1
-
+            var quizLengthIndexed = vm.questions.length - 1;
             // Loop through questions until all answered
             while (!breakOut) {
                 // Check if activeQuestion is less than length of quizz
                 // increment by 1 if it is, reset to first question if is not
                 vm.activeQuestionIndex = vm.activeQuestionIndex < quizLengthIndexed ? ++vm.activeQuestionIndex : 0;
+                // if sent back to begining warn user
+                if (vm.activeQuestionIndex === 0) {
+                    vm.error = true;
+                }
                 // Update Active Question
-                vm.activeQuestion = vm.questions[vm.activeQuestionIndex]
-                    // Stop if active question is unanswered
+                vm.activeQuestion = vm.questions[vm.activeQuestionIndex];
+                // Stop if active question is unanswered
                 if (vm.activeQuestion.answered === null) {
                     breakOut = true;
                 }
@@ -52,9 +57,19 @@
                 // Check if all questions have been answered
                 if (numQuestionsAnswered >= quizLength) {
                     // Finalize Quiz
+                    for (var i = 0; i < quizLength.length; i++) {
+                        // if there is a question unanswered send to that question
+                        if (vm.questions[i].answered === null) {
+                            switchQuestion(i);
+                            return;
+                        }
+                    }
+                    vm.error = false;
+                    vm.finalize = true;
+                    return
                 }
             }
-            vm.setActiveQuestion()
+            setActiveQuestion()
         }
 
         function selectAnswer(index) {
@@ -64,8 +79,8 @@
 
         function switchQuestion(index) {
             // Reassigns index question based on clicked icon index
-            vm.activeQuestionIndex = index
-                // Update Active Question
+            vm.activeQuestionIndex = index;
+            // Update Active Question
             vm.activeQuestion = vm.questions[vm.activeQuestionIndex]
         }
 
