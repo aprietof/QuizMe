e5822b6cb8fab9537482ed1a16e5ba42b02568cf
aprietof <adrianprieto00@gmail.com>
2016-10-17 17:16:33 -0400

    answer highlighting logic added

--- app/assets/javascripts/results/results.controller.js
+++ app/assets/javascripts/results/results.controller.js
@@ -10,21 +10,28 @@
 
         // vm Variables
         vm.answeredQuestions = QuizFactory.answeredQuestions
-        vm.correctAnswers = 0
         vm.activeResults = QuizFactory.finalize // true
+        vm.correctAnswers = 0
+        vm.activeQuestionIndex = 0
+        vm.activeQuestion = vm.answeredQuestions[vm.activeQuestionIndex] // First
 
         // Callable Methods
         vm.checkResults = checkResults;
         vm.setActiveQuestion = setActiveQuestion;
+        vm.getAnswerClass = getAnswerClass;
 
         // instatiated Functions
         checkResults()
 
         // Defined Methods
+
         function checkResults() {
+            // Loop through questions and check for correct answers
             for (var i = 0; i < vm.answeredQuestions.length; i++) {
                 if (vm.answeredQuestions[i].answer === vm.answeredQuestions[i].answered) {
+                    // if correct flag question as correct true
                     vm.answeredQuestions[i].correct = true;
+                    // then increment number of correct answers
                     vm.correctAnswers++;
                 }
             }
@@ -32,6 +39,14 @@
 
         function setActiveQuestion(index) {
 
+        }
+
+        function getAnswerClass(index) {
+            if (index !== vm.activeQuestion.answer && index === vm.activeQuestion.answered) {
+                return "answer-danger"
+            } else if (index === vm.activeQuestion.answer) {
+                return "answer-success"
+            }
         }
 
 
