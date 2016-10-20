39396d08d44e8010d1a65618ab45d410b91dcb25
aprietof <adrianprieto00@gmail.com>
2016-10-20 14:08:16 -0400

    Set up app controller to respond to CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+
+    after_filter :set_csrf_cookie
+    respond to :json
+
+    # Allows cookies with csrf token to post delete update
+    def set_csrf_cookie
+        cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+    end
+
+    def index
+    end
+
+    protected
+
+    def verified_request?
+        super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+    end
 end
