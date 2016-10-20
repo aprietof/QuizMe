60944628f965af8509e8fa5762ef5e6a9956c092
aprietof <adrianprieto00@gmail.com>
2016-10-20 14:16:43 -0400

    quiz controller added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     after_filter :set_csrf_cookie
-    respond to :json
+    respond_to :json
 
     # Allows cookies with csrf token to post delete update
     def set_csrf_cookie
---
+++ app/controllers/quiz_controller.rb
@@ -0,0 +1,50 @@
+class QuizController < ApplicationController
+
+    def index
+        quizzes = Quiz.all
+        render json: quizzes
+     end
+
+    def create
+        quiz = Quiz.new(quiz_params)
+        if quiz.save
+            render json: quiz
+        else
+            render json: { errors: quiz.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        quiz = Quiz.find(params[:id])
+        render json: quiz
+    end
+
+    def update
+        quiz = Quiz.find(params[:id])
+        if quiz.update
+            render json: quiz
+        else
+            render json: { errors: quiz.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        quiz = Quiz.find(params[:id])
+        if quiz.update(quiz_params)
+            render json: quiz
+        else
+            render json: quiz
+        end
+    end
+
+    def destroy
+        quiz = Quiz.find(params[:id])
+        render json: quiz
+    end
+
+    private
+
+    def quiz_params
+        params.require(:quiz).permit(:content)
+    end
+end
