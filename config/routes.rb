1cd48d90c89bf0ba4080b92ff6615697ea257667
aprietof <adrianprieto00@gmail.com>
2016-10-20 14:03:42 -0400

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#index'
+
+  resources :quiz, only: [:index, :show, :create, :update, :destroy]
+  resources :question, only: [:index, :show, :create, :update, :destroy]
+  resources :possibility, only: [:index, :show, :create, :update, :destroy]
 end
